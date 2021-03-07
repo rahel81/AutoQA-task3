33b2b3eacab3f779a39e5dca6e909c9502db8c90
rahel81 <rahimova81@gmail.com>
2021-03-07 22:40:37 +0300

    updated CardOrderTest v1

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -18,4 +18,48 @@ public class CardOrderTest {
         $(".button").click();
         $(".Success_successBlock__2L3Cw").shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
+
+    @Test
+    void shouldSendInvalidName() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Elena");
+        $("[data-test-id=phone] input").setValue("+79100408050");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave
+                (text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldSendInvalidPhone() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Елена");
+        $("[data-test-id=phone] input").setValue("+7910040805");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave
+                (text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldSendInvalidCheckbox() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Елена");
+        $("[data-test-id=phone] input").setValue("+79100408050");
+        //$("[data-test-id=agreement]").click();
+        $(".button").click();
+        $("[data-test-id=agreement].input_invalid").shouldHave
+                (text("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+    }
+
+    @Test
+    void shouldSendEmptyField() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("");
+        $("[data-test-id=phone] input").setValue("");
+        //$("[data-test-id=agreement]").click();
+        $(".button").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave
+                (text("Поле обязательно для заполнения"));
+    }
 }
